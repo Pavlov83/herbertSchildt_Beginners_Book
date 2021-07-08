fbe96266b1a61903f7f129669da1576b970b6332
Pavel Pavlov <pavelpa@BGSOFEFFL62-2.local>
2021-07-08 15:16:47 +0300

    creating variables

---
+++ CreateVariables.java
@@ -0,0 +1,16 @@
+class CreateVariables{
+
+    public static void main(String[] args){
+
+        int myVar1;
+        int myVar2;
+
+        myVar1 = 1024;
+        myVar2 = myVar1/2;
+
+        System.out.println("The value of myvar is" + myVar1);
+        System.out.print("My var2 is half the myvar1:" + myVar2); 
+
+    }
+
+}
