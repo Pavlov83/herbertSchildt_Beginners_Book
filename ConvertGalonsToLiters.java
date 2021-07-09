462f880716f91cd13393c695fdfb0b893a9bdcfd
Pavel Pavlov <pavelpa@BGSOFEFFL62-2.local>
2021-07-09 10:21:13 +0300

    gallons to liters converter

--- ConvertGalonsToLiters.java
+++ ConvertGalonsToLiters.java
@@ -1,5 +1,16 @@
+
 public class ConvertGalonsToLiters {
     
+    
+
+    public static void main(String[] args){
+        
+        
+        double gallonsAmount =  5;
+        double liters = gallonsAmount * 3.78;
+        System.out.println("The amount of " + gallonsAmount + " gallons is " + liters + " litters.");
+        
+    }
    
 
 }
