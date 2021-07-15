23108b6c708aca50e9b89757f6ba6e1e8b5c85dd
pavelpa <pavel.pavlov@effortel.com>
2021-07-15 15:48:28 +0300

    keyboard input

---
+++ DataTypes_and_Operators/computeDistanceOfSound.java
@@ -0,0 +1,14 @@
+package DataTypes_and_Operators;
+
+public class computeDistanceOfSound {
+    
+    public static void main(String[] args){
+        double dist;
+
+        dist = 7.2 * 1100;
+
+        System.out.println("The lightning is " + dist + "feet away.");
+    }
+
+
+}
