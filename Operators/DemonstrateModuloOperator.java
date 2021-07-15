4fdf0d06ab205a44c0ea6b6f7598543ebf23dab5
pavelpa <pavel.pavlov@effortel.com>
2021-07-15 13:20:03 +0300

    prevent divide by zero

---
+++ Operators/DemonstrateModuloOperator.java
@@ -0,0 +1,18 @@
+public class DemonstrateModuloOperator {
+
+    
+        public static void main(String[] args){
+            int iresult, irem;
+            double dresult,drem;
+
+            iresult = 10 / 3;
+            irem = 10 % 3;
+            dresult = 20 / 3.0;
+            drem = 20.0 % 3.0;
+
+            System.out.println("Result and remainder of 10 /3 is " + iresult + "Iremainder " + irem);
+            System.out.println("The remainder of  20 /3 is " + dresult + "Dremainder " + dremainder);
+            
+        }
+    
+}
