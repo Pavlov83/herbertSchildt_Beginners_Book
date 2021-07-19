34c89d82a96a1ff8b6d18a600124186e09f46c3f
pavelpa <pavel.pavlov@effortel.com>
2021-07-19 16:58:43 +0300

    added while loop

---
+++ Loops/WhileLoop/WhileLoop.java
@@ -0,0 +1,14 @@
+public class WhileLoop {
+
+    public static void main(String[] args){
+        char ch;
+
+        //print the alphabet
+        ch = 'a';
+        while(ch <= 'z'){
+            System.out.println(ch);
+            ch++;
+        }
+    }
+    
+}
