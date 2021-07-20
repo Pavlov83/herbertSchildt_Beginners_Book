bc83451ee3567d818d9336693ebcd08f2a4881a9
pavelpa <pavel.pavlov@effortel.com>
2021-07-20 13:34:10 +0300

    added Class

---
+++ Loops/DoWhileLoop/DoWhileLoop.java
@@ -0,0 +1,12 @@
+public class DoWhileLoop {
+
+    //The difference between while and do while is that do while executes
+    //at least once
+    // the construct looks like
+    //do{
+    //   statements;
+    // }while(condition)
+
+    //}
+    
+}
